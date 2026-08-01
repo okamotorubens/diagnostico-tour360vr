90d6f8ab0274f0faf3369ead6b5243fc1ab2461b
okamotorubens <okamotorubens@gmail.com>
2026-08-01 02:48:34 -0300

    Update app.py

--- app.py
+++ app.py
@@ -457,7 +457,7 @@ if btn and empresa and cidade:
                 st.success("Diagnóstico gerado com sucesso!")
 
                 nome_limpo = dados.get("name", empresa).strip()
-                file_download_name = f"Diagnóstico da Ficha - {nome_limpo}.pdf"
+                file_download_name = f"Diagnóstico da Ficha - {nome_limpo} - {cidade}.pdf"
 
                 with open(pdf_file, "rb") as f:
                     st.download_button(
