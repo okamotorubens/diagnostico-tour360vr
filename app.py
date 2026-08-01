444221f8e8d3932cb85c83192e5b052c0eeb775f
okamotorubens <okamotorubens@gmail.com>
2026-08-01 17:48:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -219,7 +219,7 @@ def gerar_pdf_bytes(dados):
             "School": "Escola",
             "Physiotherapist": "Fisioterapia",
         }
-    cats = ", ".join([traduzir(t) for t in dados.get("types", [])[:5]])
+        categorias_texto = ", ".join([traduzir(t) for t in tipos[:4]])
     
     # --- [INÍCIO DA ALTERAÇÃO] ---
     opening_hours_data = dados.get("opening_hours", {})
