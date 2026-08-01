c3641256d1c2d339883c98bf59dcd96aa05b8720
okamotorubens <okamotorubens@gmail.com>
2026-08-01 16:34:42 -0300

    Update app.py

--- app.py
+++ app.py
@@ -274,7 +274,7 @@ def gerar_pdf_bytes(dados):
     pdf.set_text_color(249, 115, 22)
     pdf.set_xy(12, y_cards + 6.0)
     score_str = str(score)
-    pdf.cell(pdf.get_string_width(score_str) + 1, 6, score_str)
+    pdf.cell(pdf.get_string_width(score_str) + 1, 6, score_str, align="C")
 
     pdf.set_font("Helvetica", "B", 18)
     pdf.set_text_color(20, 50, 135)
