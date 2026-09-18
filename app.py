9bf9d6dfaa47bb3fe5e54b21281a0714bb0c8877
okamotorubens <okamotorubens@gmail.com>
2026-09-18 02:28:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ st.markdown("""
         color: #f8fafc;
         line-height: 1.2;
         text-align: center;
-        margin: 0px 0;
+        margin: -3px 0;
         white-space: nowrap;
         letter-spacing: 0.5px;
     }
